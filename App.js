638d9544eef64b1e2bef16ade56877810babdb92
Mariem-nad <153771066+Mariem-nad@users.noreply.github.com>
2024-04-23 21:19:54 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import PlayerListe from './Components/PlayerListe';
+function App() {
+  return (
+    <div className="App">
+      <PlayerListe/>
+    </div>
+  );
+}
+
+export default App;
